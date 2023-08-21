e69fd2aa13199fe926526a7550a90a7bb5eb8541
maryam332003 <mariemgamal200333@gmail.com>
2023-08-21 17:25:22 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function demo() {
+  
+  var num1=document.getElementById('num1').value;
+  var num2=document.getElementById('num2').value;
+  var sum=Number(num1)+Number(num2)
+  document.getElementById('result').innerHTML
+}
